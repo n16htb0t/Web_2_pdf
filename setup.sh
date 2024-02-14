c94a38df5cb70b6e9b40fc64fb5ad5eedcb0398f
Daksh Khurana <132843656+n16htb0t@users.noreply.github.com>
2024-02-15 01:42:00 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    echo "Usage: $0 <Sudo password>"
+    echo "Usage: $0 <Sudo password>"https://github.com/n16htb0t/Web_2_pdf/edit/main/setup.sh
     exit 1
 fi
 
-echo $1 | sudo -S apt install ps2pdf enscript nikto docker.io python3 python3-pip
+echo $1 | sudo -S apt install -y enscript nikto docker.io python3 python3-pip 
 pip install flask
 docker run -it --rm projectdiscovery/nuclei:latest nuclei -h
 chmod +x check.sh
