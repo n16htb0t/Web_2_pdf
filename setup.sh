882475922847b6a6aa8b8ae83d7b229ed245844c
Daksh Khurana <132843656+n16htb0t@users.noreply.github.com>
2024-02-15 01:40:16 +0400

    Setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 <Sudo password>"
+    exit 1
+fi
+
+echo $1 | sudo -S apt install ps2pdf enscript nikto docker.io python3 python3-pip
+pip install flask
+docker run -it --rm projectdiscovery/nuclei:latest nuclei -h
+chmod +x check.sh
+python3 ./app.py
+
